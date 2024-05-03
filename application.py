336adff67fc602d706820de755fcba28f93d39b6
smaagoth <g.halvorsen@hotmail.com>
2024-05-03 09:34:43 +0200

    Three-way handshake established

--- application.py
+++ application.py
@@ -5,12 +5,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description='File Transfer Application')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-s', '--server', action='store_true')
-group.add_argument('-c', '--client', action='store_true')
-parser.add_argument('-f', '--file', help='Filename')
-parser.add_argument('-i', '--ip', help='IP adress')
+group.add_argument("-s", "--server", action="store_true", help="enable the server mode")
+group.add_argument("-c", "--client", action="store_true", help="enable the client mode")
+
+parser.add_argument("-i", "--ip", type=str, default="10.0.1.2",
+                        help="bind the ip address at the server side or select server's ip at the client side")
+parser.add_argument("-p", "--port", type=int, default=8088, choices=range(1024, 65536),
+                        help="select port number on which the server should listen or the server's port number at the client side")
+parser.add_argument("-f", "--file", type=str, help="choose the jpg file")
+parser.add_argument("-w", "--window", type=int, default=3, help="sliding window size")
+parser.add_argument("-d", "--discard", type=int, help="skip a seq to check for retransmission")
+
+# Parse the arguments and store them in 'args'
+args = parser.parse_args()
 
 if args.server:
-    server()
+    server(args.ip, args.port, args.file, args.window, args.discard)
 elif args.client:
-    client()
+    client(args.ip, args.port, args.file, args.window, args.discard)
+
